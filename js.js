b2af54ee1cdb22dd38a1daf64e8c0a303a924596
Gasque <75370906+LucasGasque@users.noreply.github.com>
2021-01-12 19:06:06 -0300

    atualização

---
+++ js.js
@@ -0,0 +1,14 @@
+/* alert("Em Desenvolvimento!")
+let alerta = document.getElementById("bjs")
+alerta.addEventListener('click', clicar)
+alerta.addEventListener('mouseout', sair)
+
+function clicar(){    
+    alerta.innerText = 'Em Desenvolvimento!'
+    alerta.style.textAlign = 'center'
+}
+
+function sair(){
+    return
+}
+*/
